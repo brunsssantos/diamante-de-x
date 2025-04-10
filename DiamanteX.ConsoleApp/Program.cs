fc526b6343d9ffd315946a29ca67869a6adfeb87
Bruna Santos <brunasan7os@gmail.com>
2025-04-10 19:25:47 -0300

    Implementado a solicitação de um número ímpar ao usuário

--- DiamanteX.ConsoleApp/Program.cs
+++ DiamanteX.ConsoleApp/Program.cs
@@ -6,9 +6,20 @@
         {
             int numero;
 
-           //Solicitar número impar ao usuário
-            Console.WriteLine("Digite um número inteiro ímpar: ");
-            numero = Convert.ToInt32(Console.ReadLine());
+            //Solicitar número impar ao usuário
+            do
+            {
+                Console.WriteLine("Digite um número inteiro ímpar: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("Número par. Por favor, digite um número ímpar.");
+                    Console.ReadLine();
+                }
+            }
+
+            while (numero % 2 == 0); //Repete até o usuário digitar um número ímpar
 
         }
     }

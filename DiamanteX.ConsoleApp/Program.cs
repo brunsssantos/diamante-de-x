eac131d135d925e3fc170c7cde49a6b76873f4aa
Bruna Santos <brunasan7os@gmail.com>
2025-04-10 19:32:43 -0300

    Implementação do diamante de X

--- DiamanteX.ConsoleApp/Program.cs
+++ DiamanteX.ConsoleApp/Program.cs
@@ -12,7 +12,8 @@
                 Console.WriteLine("Digite um número inteiro ímpar: ");
                 numero = Convert.ToInt32(Console.ReadLine());
 
-                if (numero % 2 == 0)
+                if (numero % 2 == 0)//Se digitar um número par
+
                 {
                     Console.WriteLine("Número par. Por favor, digite um número ímpar.");
                     Console.ReadLine();
@@ -21,6 +22,30 @@
 
             while (numero % 2 == 0); //Repete até o usuário digitar um número ímpar
 
+            //Gerar o diamante
+
+            int meio = numero / 2; //Linha central do diamante
+            for (int i = 0; i < numero; i++)
+            {
+                //Determinar número de X na linha
+                int qtdXs;
+
+                if (i <= meio)
+                {
+                    qtdXs = 1 + 2 * i;
+                }
+                else
+                {
+                    qtdXs = 1 + 2 * (numero - 1 - i);
+                }
+
+                int espacos = (numero - qtdXs) / 2;
+
+                Console.Write(new string(' ', espacos));
+                Console.WriteLine(new string('X', qtdXs));
+
+                Console.ReadLine();
+            }
         }
     }
 }

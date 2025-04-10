c8afde846c7e5fbfb1ae6e1907f79130127a4b13
Bruna Santos <brunasan7os@gmail.com>
2025-04-10 19:22:18 -0300

    Adicionar arquivos de projeto.

---
+++ DiamanteX.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DiamanteX.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+           //Solicitar número impar ao usuário
+            Console.WriteLine("Digite um número inteiro ímpar: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
